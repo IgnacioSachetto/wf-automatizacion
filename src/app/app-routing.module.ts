22227b11b22debcf79c93f4b0d0df760322c024c
IgnacioSachetto <nachosachetto1998@hotmail.com>
2025-01-09 12:41:36 -0300

    commit: IA WF - V.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormularioCargaRiesgoComponent } from './formulario-carga-riesgo/formulario-carga-riesgo.component';
+import { FormularioNuevaIniciativaIaComponent } from './formulario-nueva-iniciativa-ia/formulario-nueva-iniciativa-ia.component';
 import { FormularioIniciativaComponent } from './formulario-nueva-iniciativa/formulario-nueva-iniciativa.component';
 import { PantallaIntermediaIaComponent } from './pantalla-intermedia-ia/pantalla-intermedia-ia.component';
 import { PantallaIntermediaProductoComponent } from './pantalla-intermedia-producto/pantalla-intermedia-producto.component';
+
 
 
 const routes: Routes = [
@@ -11,6 +13,7 @@ const routes: Routes = [
   { path: 'pantalla-intermedia-ia', component: PantallaIntermediaIaComponent },
   { path: 'formulario-nueva-iniciativa', component: FormularioIniciativaComponent },
   { path: 'formulario-carga-riesgo', component: FormularioCargaRiesgoComponent },
+  { path: 'formulario-nueva-iniciativa-ia', component: FormularioNuevaIniciativaIaComponent},
 
   { path: '', redirectTo: '', pathMatch: 'full' }, // No redirige a ninguna pantalla, se queda en blanco
 ];
--- src/app/formulario-nueva-iniciativa-ia/formulario-nueva-iniciativa-ia.component.ts
+++ src/app/formulario-nueva-iniciativa-ia/formulario-nueva-iniciativa-ia.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./formulario-nueva-iniciativa-ia.component.css']
 })
 export class FormularioNuevaIniciativaIaComponent {
+  mostrarPreguntaProveedor: boolean = false;
+
+  onTipoIniciativaChange(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    const tipoIniciativa = selectElement.value;
+
+    if (tipoIniciativa === 'externo') {
+      this.mostrarPreguntaProveedor = true;
+    } else {
+      this.mostrarPreguntaProveedor = false;
+    }
+  }
 
 }
